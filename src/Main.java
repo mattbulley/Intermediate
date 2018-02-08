bf202c614a55b0ce91e159d62580df4c2b154928
mattbulley <mattbulley01@gmail.com>
2018-02-08 15:02:02 +0000

    setting up Junit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -120,6 +123,28 @@ public class Main {
 			
 		}
 		
+		System.out.println("");
+		System.out.println("New array list being made");
+		List<Person> personArrayList2 = new ArrayList<Person>();
+		
+		
+		personArrayList2.add(readFile("Matt.txt"));
+		System.out.println("reading Matt.txt");
+		personArrayList2.add(readFile("Bob.txt"));
+		System.out.println("reading Bob.txt");
+		personArrayList2.add(readFile("Steve.txt"));
+		System.out.println("reading Steve.txt");
+		personArrayList2.add(readFile("Amy.txt"));
+		System.out.println("reading Amy.txt");
+		personArrayList2.add(readFile("Joe.txt"));
+		System.out.println("reading Joe.txt");
+		
+		for(Person person : personArrayList2) {
+			System.out.println("NAME: " + person.name + " AGE: " + person.age + " JOB: " + person.jobTitle);
+		
+			
+		}
+		
 	}
 	
 	public static void writeFile(String _name, int _age, String _job, String _fileName) {
@@ -140,8 +165,69 @@ public class Main {
 		
 	}
 	
-	public static void readFile(String _fileName) {
+	public static Person readFile(String _fileName) {
 		
+			String fileName = _fileName;
+
+	        // This will reference one line at a time
+	        String line = null;
+	        String[] available = new String[3];
+	        int i = 0;
+	        String name = "";
+	        String age = "";
+	        String job = "";
+	        
+	        try {
+	            // FileReader reads text files in the default encoding.
+	            FileReader fileReader = 
+	                new FileReader(fileName);
+
+	            // Always wrap FileReader in BufferedReader.
+	            BufferedReader bufferedReader = 
+	                new BufferedReader(fileReader);
+
+	            while((line = bufferedReader.readLine()) != null) {
+	            	
+	                available[i] = line;
+	              //  System.out.println(i + " " + available[i]);
+	                switch (i) {
+	                	case 0:
+	                		name = available[i];
+	                		break;
+	                	case 1:
+	                		age = available[i];
+	                		break;
+	                	case 2:
+	                		job = available[i];
+	                		break;
+	                
+	                }
+	                
+	                
+	                
+	                i++;
+	                
+	            }   
+
+	            // Always close files.
+	            bufferedReader.close();         
+	        }
+	        catch(FileNotFoundException ex) {
+	            System.out.println(
+	                "Unable to open file '" + 
+	                fileName + "'");                
+	        }
+	        catch(IOException ex) {
+	            System.out.println(
+	                "Error reading file '" 
+	                + fileName + "'");                  
+	            // Or we could just do this: 
+	            // ex.printStackTrace();
+	        }
+	        
+	        Person person = new Person(name, Integer.parseInt(age), job);
+	        
+	        return person;
 	}
 	
 	public static int BlackJack(int val1, int val2)
